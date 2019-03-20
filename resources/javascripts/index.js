d83bce213061a2dd41a14f181386eead1831262a
Dawn <tangcaiyuan@hust.edu.cn>
2019-03-20 16:31:58 +0800

    Revert "Revert "前后端整合完成""
    This reverts commit 64de9fbccd1fc9b29261f8202e662ce44a65b69e.

--- resources/javascripts/index.js
+++ resources/javascripts/index.js
@@ -1,13 +1,24 @@
-/*
-$(function() {
-    $('#menu-toggle').click(function(e) {
+$(function () {
+    $("#menu-toggle").click(function(e) {
         e.preventDefault();
-        $('#wrapper').toggleClass('toggled');
+        $("#wrapper").toggleClass("toggled");
     });
 
-    (function() {
+    (function () {
         var pathname = window.location.pathname.substring(1);
         $('#sidebar-wrapper a[href="' + pathname + '"]').addClass('active');
     })();
+
+    // $(".nav li .active").each(function(index) {
+    //     $(".nav  li .active").eq(index).addClass("aClicked");
+    // })
+
+    // $(".nav li a.active").each(function(index) {
+    //     $(this).click(function() {
+    //         $(".nav  li").eq(index).addClass("aClicked").siblings().removeClass("navClicked");
+    //     })
+    // })
+
 });
-*/
+
+$('#myCarousel').carousel('pause')
